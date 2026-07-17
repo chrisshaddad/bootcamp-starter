304ad026977c5aef31b31e3d918dab76ae71fa71
Mohamad Farchoukh <mohammad.farchoukh55@gmail.com>
2026-07-17 22:57:10 +0300

    Add course management API endpoints

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -11,6 +11,8 @@ import { UsersModule } from './users/users.module';
 import { TeacherModule } from './teacher/teacher.module';
 import { StudentsModule } from './students/students.module';
 import { TeachersModule } from './teachers/teachers.module';
+import { CoursesModule } from './courses/courses.module';
+import { CoursesService } from './courses/courses.service';
 
 @Module({
   imports: [
@@ -28,6 +30,7 @@ import { TeachersModule } from './teachers/teachers.module';
     TeacherModule,
     StudentsModule,
     TeachersModule,
+    CoursesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
