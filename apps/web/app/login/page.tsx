0ec75d28fcc09e0f853b5eca858d220763cdbb09
chko0 <chadi.kouzayha@gmail.com>
2026-07-19 17:08:36 +0300

    Fix login registration link and copy

--- apps/web/app/login/page.tsx
+++ apps/web/app/login/page.tsx
@@ -150,11 +150,14 @@ export default function LoginPage() {
             {/* Register Link */}
             <p className="text-center text-sm font-medium leading-[1.6]">
               <span className="text-gray-500">
-                Don&apos;t have a organization?{' '}
+                Don&apos;t have an organization?{' '}
               </span>
-              <a href="/register" className="text-primary-base hover:underline">
+              <Link
+                href="/register"
+                className="text-primary-base hover:underline"
+              >
                 Create an Org
-              </a>
+              </Link>
             </p>
           </div>
         </div>
